fe2a4a36ac65abb158140fa4ba25711a127c45db
Richard Schneider <richard@schneiderbox.net>
2022-02-19 17:25:38 -0600

    Render worker locations (primitively)

--- ui/js/erastus.js
+++ ui/js/erastus.js
@@ -2,9 +2,16 @@ const e = React.createElement;
 
 
 function cleanHash() {
-    if (location.hash.length != 31) {
-        location.hash = '0000000000000000000000000xxxx0';
+    if (location.hash.length != 35) {
+        location.hash = '0000000000000000000000000xxxxxxxx0';
     }
+}
+
+
+function coordToSpace(coord) {
+    col = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4}[coord[0].toLowerCase()];
+    row = parseInt(coord[1]) - 1;
+    return 5*row + col;
 }
 
 
@@ -13,19 +20,40 @@ class Space extends React.Component {
         let className = 'col space ';
         className += ' level-' + this.props.level;
 
-        return e('button', {className: className});
+        return e('button', {className: className}, this.props.worker);
     }
 }
 
 
 class Board extends React.Component {
     render() {
+        let p1WorkerCoords = [
+            coordToSpace(this.props.notation.slice(25, 28)),
+            coordToSpace(this.props.notation.slice(27, 30)),
+        ];
+        let p2WorkerCoords = [
+            coordToSpace(this.props.notation.slice(29, 32)),
+            coordToSpace(this.props.notation.slice(31, 34)),
+        ];
+
         let rows = [];
         for (let i = 0; i < 5; i++) {
             let row = [];
             for (let j = 0; j < 5; j++) {
+                let space = 5*i + j;
+
+                let worker = null;
+                console.log(space, p1WorkerCoords);
+                if (p1WorkerCoords.includes(space)) {
+                    worker = 1;
+                }
+                if (p2WorkerCoords.includes(space)) {
+                    worker = 2;
+                }
+
                 row.push(e(Space, {key: j,
-                    level: parseInt(this.props.notation[1 + 5*i + j]),
+                    level: parseInt(this.props.notation[space]),
+                    worker: worker,
                 }));
             }
 
@@ -49,7 +77,7 @@ class UI extends React.Component {
     }
 
     render() {
-        return e(Board, {notation: location.hash});
+        return e(Board, {notation: location.hash.slice(1)});
     }
 }
 
