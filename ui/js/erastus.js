56023e804a0504b130a693d91b6b8fb19bc1b714
Richard Schneider <richard@schneiderbox.net>
2022-03-09 17:16:28 -0600

    Rework UI to use the API's listing of moves

--- ui/js/erastus.js
+++ ui/js/erastus.js
@@ -1,11 +1,7 @@
+API_URL = 'http://127.0.0.1:5000'
+
+
 const e = React.createElement;
-
-
-function cleanHash() {
-    if (location.hash.length != 35) {
-        location.hash = '0000000000000000000000000xxxxxxxx1';
-    }
-}
 
 
 function coordToSpace(coord) {
@@ -15,59 +11,118 @@ function coordToSpace(coord) {
 }
 
 
+function spaceToCoord(spacei) {
+    col = 'abcde'[spacei % 5];
+    row = Math.floor(spacei / 5) + 1;
+    return col + row;
+}
+
+
 class Space extends React.Component {
     render() {
-        let className = 'col fs-1 space ';
+        let className = 'col fs-1 space';
         className += ' level-' + this.props.level;
+
+        if (this.props.inInputBuffer) {
+            className += ' inputBuffer';
+        }
 
         return e('button', {
             className: className,
             disabled: !this.props.active,
+            onClick: this.props.handleClick.bind(this, this.props.spacei),
         }, ['', 'X', 'O'][this.props.worker]);
     }
 }
 
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            inputBuffer: [],
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+
+    isPlacePhase() {
+        return this.props.notation.toLowerCase().includes('x');
+    }
+
+
+    turn() {
+        return parseInt(this.props.notation.slice(33, 34));
+    }
+
+
+    buildActionTree() {
+        let actionTree = {};
+
+        for (let action of this.props.actions) {
+            let actionSpaces = action.split(/\&|-|\+/).map(coordToSpace);
+
+            if (actionTree[actionSpaces[0]] === undefined) {
+                actionTree[actionSpaces[0]] = {};
+            }
+
+            if (actionTree[actionSpaces[0]][actionSpaces[1]] === undefined) {
+                if (actionSpaces.length === 2) {
+                    actionTree[actionSpaces[0]][actionSpaces[1]] = null;
+                } else {
+                    actionTree[actionSpaces[0]][actionSpaces[1]] = {};
+                }
+            }
+
+            if (actionSpaces.length === 3) {
+                actionTree[actionSpaces[0]][actionSpaces[1]][actionSpaces[2]] = null;
+            }
+        }
+
+        return actionTree;
+    }
+
+
     render() {
-        let p1WorkerCoords = [
+        let p1WorkerSpaces = [
             coordToSpace(this.props.notation.slice(25, 27)),
             coordToSpace(this.props.notation.slice(27, 29)),
         ];
-        let p2WorkerCoords = [
+        let p2WorkerSpaces = [
             coordToSpace(this.props.notation.slice(29, 31)),
             coordToSpace(this.props.notation.slice(31, 33)),
         ];
 
-        let turn = parseInt(this.props.notation.slice(33, 34));
-
-        let placePhase = this.props.notation.toLowerCase().includes('x');
+        let actionTree = this.buildActionTree();
 
         let rows = [];
         for (let i = 0; i < 5; i++) {
             let row = [];
             for (let j = 0; j < 5; j++) {
-                let space = 5*i + j;
+                let spacei = 5*i + j;
 
                 let worker = null;
-                if (p1WorkerCoords.includes(space)) {
+                if (p1WorkerSpaces.includes(spacei)) {
                     worker = 1;
-                }
-                if (p2WorkerCoords.includes(space)) {
+                } else if (p2WorkerSpaces.includes(spacei)) {
                     worker = 2;
                 }
 
-                let active;
-                if (placePhase) {
-                    active = worker === null;
-                } else {
-                    active = turn === worker;
+                let activeSpaces = actionTree;
+                for (let inputi of this.state.inputBuffer) {
+                    activeSpaces = activeSpaces[inputi];
                 }
+                let active = activeSpaces[spacei] !== undefined;
 
                 row.push(e(Space, {key: j,
-                    level: parseInt(this.props.notation[space]),
+                    spacei: spacei,
+                    handleClick: this.handleClick,
+                    level: parseInt(this.props.notation[spacei]),
                     worker: worker,
                     active: active,
+                    inInputBuffer: this.state.inputBuffer.includes(spacei),
                 }));
             }
 
@@ -76,6 +131,24 @@ class Board extends React.Component {
 
         return e('div', {className: 'board'}, rows);
     }
+
+    handleClick(spacei, e) {
+        let buffer = this.state.inputBuffer.slice();
+        buffer.push(spacei);
+
+        let activeSpaces = this.buildActionTree();
+        for (let inputi of buffer) {
+            activeSpaces = activeSpaces[inputi];
+        }
+
+        if (activeSpaces === null) {
+            // TODO submit move to server
+            console.log('move complete');
+            return;
+        }
+
+        this.setState({inputBuffer: buffer});
+    }
 }
 
 
@@ -83,15 +156,42 @@ class UI extends React.Component {
     constructor(props) {
         super(props);
 
-        cleanHash();
-        window.addEventListener('hashchange', () => {
-            cleanHash();
-            this.forceUpdate();
-        });
+        this.state = {
+            actions: [],
+        };
+
+        this.hashChange = this.hashChange.bind(this);
+        window.addEventListener('hashchange', this.hashChange);
+    }
+
+    componentDidMount() {
+        this.hashChange();
     }
 
     render() {
-        return e(Board, {notation: location.hash.slice(1)});
+        return e(Board, {
+            notation: location.hash.slice(1),
+            actions: this.state.actions,
+        });
+    }
+
+    hashChange() {
+        // TODO better checks here
+        if (location.hash.length != 35) {
+            location.hash = '0000000000000000000000000xxxxxxxx1';
+            return;
+        }
+        // TODO clear inputBuffer
+
+        // When the hash changes, get the actions from the server
+        this.setState({actions: []});
+        fetch(API_URL + '/actions/' + location.hash.slice(1))
+            .then(response => response.json())
+            .then(json => {
+                console.log(json.log);
+                this.setState({actions: json.actions});
+            })
+            .catch(console.error);
     }
 }
 
